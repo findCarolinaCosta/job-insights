f9a4e9bcd818f2e353d38170a574558f81cbaf04
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-01 20:37:39 -0300

    Feat: filter_by_salary_range function

--- src/insights.py
+++ src/insights.py
@@ -181,4 +181,14 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+
+    filtered = []
+
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered.append(job)
+        except ValueError:
+            pass
+
+    return filtered
