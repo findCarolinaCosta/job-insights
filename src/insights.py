1a69968ef7d1741a286ebbf8d197ffced8bd5ef2
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-01 19:13:52 -0300

    Feat: get_min_salary function

--- src/insights.py
+++ src/insights.py
@@ -119,7 +119,15 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+
+    return min(
+        [
+            int(salary["min_salary"])
+            for salary in jobs
+            if salary["min_salary"].isnumeric()
+        ]
+    )
 
 
 def matches_salary_range(job, salary):
