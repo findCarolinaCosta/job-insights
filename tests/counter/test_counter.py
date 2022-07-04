c4dc6e93d3e19f3a33746ed718bef6ad8971290a
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-04 20:41:26 -0300

    Feat: test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+import pytest
 
 
-def test_counter():
-    pass
+@pytest.fixture(scope="session")
+def fake_data():
+    return "python cs Python Cs PYTHON"
+
+
+def test_counter(fake_data):
+    with patch("builtins.open", mock_open(read_data=fake_data)):
+        assert count_ocurrences("", "python") == 3
+        assert count_ocurrences("", "cs") == 2
