4c7393b8973ea5bc556857b4aa74ec075eaa67f3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-04 21:14:04 -0300

    Feat: test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+from unittest.mock import mock_open, patch
+import pytest
 
 
-def test_brazilian_jobs():
-    pass
+@pytest.fixture(scope="session")
+def fake_data():
+    return """titulo,salario,tipo
+Maquinista,2000,trainee"""
+
+
+def test_brazilian_jobs(fake_data):
+    with patch("builtins.open", mock_open(read_data=fake_data)):
+        assert read_brazilian_file("") == [
+            {
+                "title": "Maquinista",
+                "salary": "2000",
+                "type": "trainee",
+            }
+        ]
